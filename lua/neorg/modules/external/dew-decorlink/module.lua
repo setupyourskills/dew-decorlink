5bf12e6aa4ebe6277a909f76f2028d63b6220406
Thubten Rigzin <thubtenrigzin@gmail.com>
2025-07-22 22:32:47 +0800

    refactor: add local alias for 'external.neorg-dew' module import and 'api' for 'vim.api'

--- lua/neorg/modules/external/dew-decorlink/module.lua
+++ lua/neorg/modules/external/dew-decorlink/module.lua
@@ -1,5 +1,6 @@
 local neorg = require "neorg.core"
 local modules = neorg.modules
+local neorg_dew = require("neorg.core.modules").get_module "external.neorg-dew"
 
 local api = vim.api
 
@@ -49,51 +50,45 @@ module.private = {
   end,
 
   pick_and_insert_icon = function()
-    require("neorg.core.modules")
-      .get_module("external.neorg-dew")
-      .telescope_picker("Choose an iten to insert", module.private.set_formatted_icon_list(), {
-        entry_value = function(entry)
-          return entry
-        end,
-        entry_display = function(entry)
-          return entry
-        end,
-        entry_ordinal = function(entry)
-          return entry
-        end,
-      }, function(map, action_state, actions)
-        map("i", "<CR>", function(bufnr)
-          local selection = action_state.get_selected_entry()
-          actions.close(bufnr)
+    neorg_dew.telescope_picker("Choose an iten to insert", module.private.set_formatted_icon_list(), {
+      entry_value = function(entry)
+        return entry
+      end,
+      entry_display = function(entry)
+        return entry
+      end,
+      entry_ordinal = function(entry)
+        return entry
+      end,
+    }, function(map, action_state, actions)
+      map("i", "<CR>", function(bufnr)
+        local selection = action_state.get_selected_entry()
+        actions.close(bufnr)
 
-          local icon = module.private.extract_icon(selection.display)
-          -- api.nvim_put({ icon }, "c", true, true)
+        local icon = module.private.extract_icon(selection.display)
 
-          local line = vim.api.nvim_get_current_line()
+        local line = api.nvim_get_current_line()
 
-          -- Trouve la position du premier "[" (tu peux changer en dernier si besoin)
-          local insert_pos = line:find("[", 1, true)
+        local insert_pos = line:find("[", 1, true)
 
-          if insert_pos then
-            -- Reconstruire la ligne avec l'icône insérée juste après le [
-            local before = line:sub(1, insert_pos)
-            local after = line:sub(insert_pos + 1)
-            local new_line = before .. icon .. " " .. after
+        if insert_pos then
+          local before = line:sub(1, insert_pos)
+          local after = line:sub(insert_pos + 1)
+          local new_line = before .. icon .. " " .. after
 
-            vim.api.nvim_set_current_line(new_line)
-          else
-            -- Aucun "[", on n'insère rien (ou tu peux insérer ailleurs si tu veux)
-            vim.notify("No '[' found on the current line to insert icon", vim.log.levels.WARN)
-          end
-        end)
-        return true
+          api.nvim_set_current_line(new_line)
+        else
+          vim.notify("No '[' found on the current line to insert icon", vim.log.levels.WARN)
+        end
       end)
+      return true
+    end)
   end,
 }
 
 module.on_event = function(event)
   if event.split_type[2] == "dew-decorlink.insert" then
-    local current_line = vim.api.nvim_get_current_line()
+    local current_line = api.nvim_get_current_line()
 
     if current_line:find "}" then
       module.private.pick_and_insert_icon()
